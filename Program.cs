f6df51d5695b513e0798788767b76ab0003bfc1d
JuanaVillaba <137566713+JuanaVillaba@users.noreply.github.com>
2025-04-19 12:45:09 -0300

    Ejecicio1y2.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Holaaaaaaaa");
             Auto auto = new Auto();
             auto.Arrancar();
             Moto moto = new Moto();
